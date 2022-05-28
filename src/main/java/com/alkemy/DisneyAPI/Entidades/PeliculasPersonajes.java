0e2f3ca9a1828b88627470c2f5d15fb402d791e5
Franco940 <franco.3271940@gmail.com>
2022-05-27 21:49:59 -0300

    Se creo esta entidad para mapear correctamente la información de las peliculas a la hora de mostrarlas. Y también se utiliza para hacer las relaciones en la base de datos.

---
+++ src/main/java/com/alkemy/DisneyAPI/Entidades/PeliculasPersonajes.java
@@ -0,0 +1,45 @@
+package com.alkemy.DisneyAPI.Entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Franco Roman
+ */
+
+@Entity
+@Table(name = "peliculas_personaje")
+public class PeliculasPersonajes {
+    @Id
+    private Long id;
+    private String nombre;
+    
+    @Lob
+    private byte[] imgaenPersonaje;
+
+    public PeliculasPersonajes() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getImgaenPersonaje() {
+        return imgaenPersonaje;
+    }
+
+    public void setImgaenPersonaje(byte[] imgaenPersonaje) {
+        this.imgaenPersonaje = imgaenPersonaje;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
