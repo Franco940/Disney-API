3fd12c4852645e1f7c74a7f6c196857476d5c5df
Franco940 <franco.3271940@gmail.com>
2022-05-29 23:43:54 -0300

    Se eliminaron metodos obsoletos

--- src/main/java/com/alkemy/DisneyAPI/Servicios/UsuarioServicio.java
+++ src/main/java/com/alkemy/DisneyAPI/Servicios/UsuarioServicio.java
@@ -2,14 +2,7 @@ package com.alkemy.DisneyAPI.Servicios;
 
 import com.alkemy.DisneyAPI.Entidades.Usuario;
 import com.alkemy.DisneyAPI.Repositorios.UsuarioRepositorio;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -50,16 +43,6 @@ public class UsuarioServicio{
         }
     }
     
-    public UserDetails cargarUsuario(String nombre) throws UsernameNotFoundException {
-        Usuario usuario = repositorioUsuario.buscarUsuarioPorNombre(nombre);
-        
-        if(usuario.getNombre().equals("ADMIN")){
-            return this.userBuilder(usuario.getNombre(), passwordEncoder.encode(usuario.getContrasenia()), usuario.getRol());
-        }else{
-            return this.userBuilder(usuario.getNombre(), passwordEncoder.encode(usuario.getContrasenia()), usuario.getRol());
-        }
-    }
-    
     private void validaciones(String nombre, String contrasenia, String email) throws Exception{
         if(nombre == null || nombre.isEmpty()){
             throw new Exception("El nombre no puede estar vacío");
@@ -70,21 +53,5 @@ public class UsuarioServicio{
         if(email == null || email.isEmpty()){
             throw new Exception("El email no puede estar vacio");
         }
-    }
-    
-    // Clase usuario de spring
-    private User userBuilder(String username, String password, String... roles){
-        boolean enabled = true;
-        boolean accountNonExpired = true;
-        boolean credentialsNonExpired = true;
-        boolean accountNonLocked = true;
-        
-        List<GrantedAuthority> authorities = new ArrayList();
-        
-        for(String role : roles){
-            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
-        }
-        
-        return new User(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
     }
 }
